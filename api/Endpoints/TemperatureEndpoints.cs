77c758743c72eb47dad33a33ad11ddbe6a2d171c
PanSzelescik <panszelescik@gmail.com>
2025-05-17 16:13:26 +0200

    Add some endpoints

--- api/Endpoints/TemperatureEndpoints.cs
+++ api/Endpoints/TemperatureEndpoints.cs
@@ -18,6 +18,9 @@ public static class TemperatureEndpoints
 
         group.MapGet("{deviceId}", DeviceTemperatures);
         group.MapGet("{deviceId}/current", CurrentDeviceTemperature);
+        group.MapGet("{deviceId}/average", AverageDeviceTemperature);
+        group.MapGet("{deviceId}/min", MinDeviceTemperature);
+        group.MapGet("{deviceId}/max", MaxDeviceTemperature);
     }
     
     private static async Task<Ok<List<TemperatureEntity>>> DeviceTemperatures(
@@ -44,11 +47,69 @@ public static class TemperatureEndpoints
     {
         var userId = httpContext.GetUserId();
         
-        var temperature = await db.Temperatures
+        var temperatureEntity = await db.Temperatures
             .Where(x => x.DeviceId == deviceId)
             .OrderByDescending(x => x.CreatedDate)
             .FirstOrDefaultAsync(cancellationToken);
 
-        return temperature == null ? TypedResults.NotFound() : TypedResults.Ok(temperature);
+        return temperatureEntity == null ? TypedResults.NotFound() : TypedResults.Ok(temperatureEntity);
     }
+    
+    private static async Task<Results<Ok<double>, NotFound>> AverageDeviceTemperature(
+        [FromRoute] string deviceId,
+        [FromServices] ApplicationDbContext db,
+        HttpContext httpContext,
+        CancellationToken cancellationToken)
+    {
+        var userId = httpContext.GetUserId();
+        
+        var date = DateTimeOffset.UtcNow.AddDays(-7);
+
+        var temperature = await db.Temperatures
+            .Where(x => x.DeviceId == deviceId && x.CreatedDate >= date)
+            .Select(x => (double?)x.State) // <- rzutowanie na nullable
+            .DefaultIfEmpty()
+            .AverageAsync(cancellationToken);
+        
+        return temperature == null ? TypedResults.NotFound() : TypedResults.Ok(temperature.Value);
+    }
+    
+    private static async Task<Results<Ok<double>, NotFound>> MinDeviceTemperature(
+        [FromRoute] string deviceId,
+        [FromServices] ApplicationDbContext db,
+        HttpContext httpContext,
+        CancellationToken cancellationToken)
+    {
+        var userId = httpContext.GetUserId();
+        
+        var date = DateTimeOffset.UtcNow.AddDays(-7);
+
+        var temperature = await db.Temperatures
+            .Where(x => x.DeviceId == deviceId && x.CreatedDate >= date)
+            .Select(x => (double?)x.State) // <- rzutowanie na nullable
+            .DefaultIfEmpty()
+            .MinAsync(cancellationToken);
+        
+        return temperature == null ? TypedResults.NotFound() : TypedResults.Ok(temperature.Value);
+    }
+    
+    private static async Task<Results<Ok<double>, NotFound>> MaxDeviceTemperature(
+        [FromRoute] string deviceId,
+        [FromServices] ApplicationDbContext db,
+        HttpContext httpContext,
+        CancellationToken cancellationToken)
+    {
+        var userId = httpContext.GetUserId();
+        
+        var date = DateTimeOffset.UtcNow.AddDays(-7);
+
+        var temperature = await db.Temperatures
+            .Where(x => x.DeviceId == deviceId && x.CreatedDate >= date)
+            .Select(x => (double?)x.State) // <- rzutowanie na nullable
+            .DefaultIfEmpty()
+            .MaxAsync(cancellationToken);
+        
+        return temperature == null ? TypedResults.NotFound() : TypedResults.Ok(temperature.Value);
+    }
+
 }
